0c0324dc2f8ce67ed14dfc84109a9f73ba1b1f79
任欣源 <245735349@qq.com>
2017-09-14 13:18:23 +0800

    多进程pool
    close()  不允许添加新的进程
    join()  等待所有子进程运行完

---
+++ procedure/process_pool.py
@@ -0,0 +1,23 @@
+from multiprocessing import Pool
+import os, time, random
+
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    #不能继续添加新的进程
+    p.close()
+    #等待所有子进程运行完成
+    p.join()
+    print('All subprocesses done.')
