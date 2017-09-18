d41a3ce39f0cd8383a005e3a3186416eb9ffd8d5
任欣源 <245735349@qq.com>
2017-09-18 09:00:30 +0800

    多线程如何操作全局变量
    类似自定义字典
    threadlocal

---
+++ thread_local/thread_local.py
@@ -0,0 +1,28 @@
+import threading
+
+# 创建全局ThreadLocal对象:
+local_school = threading.local()
+
+
+def process_student():
+    # 获取当前线程关联的student:
+    std = local_school.student
+    print('Hello, %s (in %s)' % (std, threading.current_thread().name))
+
+
+def process_thread(name):
+    # 绑定ThreadLocal的student:
+    local_school.student = name
+    process_student()
+
+
+t1 = threading.Thread(target=process_thread, args=('Alice',), name='Thread-A')
+t2 = threading.Thread(target=process_thread, args=('Bob',), name='Thread-B')
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+#全局变量local_school就是一个ThreadLocal对象，每个Thread对它都可以读写student属性，但互不影响。
+#可以把local_school看成全局变量，但每个属性如local_school.student都是线程的局部变量，
+# 可以任意读写而互不干扰，也不用管理锁的问题，ThreadLocal内部会处理。
